0faa5b58733ed6e7c9daa254bed0dbc09ccca8c2
arunbharti-1 <114228660+arunbharti-1@users.noreply.github.com>
2024-06-30 07:22:39 +0000

    Array

--- STL/array.cpp
+++ STL/array.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //STL-Array in c++
 int main()
 {
+    /*
     array<int,5> a={10,20,30,40,50};
     array<int,5> b={100,200,300,400,500};
     cout<<"Array Size "<<a.size()<<endl;
@@ -15,6 +16,9 @@ int main()
     cout<<"Array First Element :"<<a.front()<<endl;
     cout<<"Array Last Element :"<<a.back()<<endl;
     cout<<"Before Swap :"<<endl;
+
+
+    //this is read only for(auto x :a)
     cout<<"A :";
     for(int x :a){cout<<x<<" ";}cout<<endl;
     cout<<"B :";
@@ -29,6 +33,106 @@ int main()
     c.fill(10);
     cout<<"C : ";
     for(int x: c){cout<<x<<" ";}cout<<endl;
+    */
+
+
+
+
+    //pankaj sir
+    // array<int,6>a = {10,20,30,40,50,60};
+    // for(int i = 0 ; i < a.max_size() ;i++)
+    // cout<<a[i]<<" ";
+    // cout<<endl;
+    // for(int i = 0 ; i < a.max_size() ;i++)
+    // {
+    //     a[i] = 0;
+    // }
+    // cout<<endl;
+    // for(int i = 0 ; i < a.max_size() ;i++)
+    // cout<<a[i]<<" ";
+
+    //array<int,5>b = {1,2,3,4,5};
+    // for(auto i:b)
+    // cout<<i<<" ";
+    // cout<<endl;
+    //only read only not updated
+    // for(auto i : b){
+    //     b[i] = 0;
+    // }
+    // cout<<endl;
+    // for(auto i:b)
+    // cout<<i<<" ";
+    array<int,6>a = {10,20,30,40,50,60};
+    array<int , 6>::iterator it;
+    it = a.begin();
+    cout<<*it;
+    cout<<endl;
+    for(;it != a.end() ;it++)
+    cout<<*it<<" ";
+
+    cout<<endl;
+    for(;it != a.end() ;it++)
+    {
+        *it = *it+1;
+    }
+
+    cout<<endl;
+
+    for(int x : a)
+    cout<<x<<" ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 
 }
