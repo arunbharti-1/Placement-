9b009375096aa00efa32bfc6eec420ce3a1c03c7
arunbharti-1 <114228660+arunbharti-1@users.noreply.github.com>
2024-07-04 05:33:26 +0000

    4JULY

---
+++ coding problems/cp_1.cpp
@@ -0,0 +1,15 @@
+//sum of array using recursion
+#include<iostream>
+using namespace std;
+int findsum(int a[] , int size)
+{
+    if(size == 0)
+    return 0;
+return a[0]+findsum(a+1,size-1);
+}
+int main()
+{
+    int a[] = {12,3,45,67,9};
+    cout<<"Sum of arrays is: "<<findsum(a,5)<<endl;
+    return 0;
+}
